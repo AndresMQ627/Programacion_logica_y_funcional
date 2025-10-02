c2695560437db54423e0658b042565584b6a8ff8
AndresMQ627 <andres.190822@gmail.com>
2025-10-02 12:59:03 -0700

    a

--- javaScript/introduccion.js
+++ javaScript/introduccion.js
@@ -10,3 +10,35 @@ const checkEstado = (edad, edadLegal)=>{
 
 const persona = checkEstado(18, edaLegalEEUU)
 console.log(persona)
+
+let arreglo = [
+
+    {id:1, name:"Luis", age:20},
+    {id:2, name:"Sebastiano", age:21},
+    {id:3, name:"Anoon", age:20},
+    {id:4, name:"Andres", age:21},
+]
+let ul = document.getElementById("lista") 
+
+var todo = ""
+
+arreglo.forEach((item)=>{
+    item.age = item.age*10
+    todo += `
+         <li>${item.name} - ${item.age}</li>
+    `
+})
+
+ul.innerHTML = todo
+
+
+
+console.log("arreglo original", arreglo);
+var copia = arreglo.map((item, index)=>{
+     item.age *= 10
+        return item
+})
+
+console.log("arreglo modificado", arreglo);
+console.log("arreglo modificado", copia);
+
