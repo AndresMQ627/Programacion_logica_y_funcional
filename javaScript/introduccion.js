754d6aa8ca53a7b67af2a3d9b27800d075dcf4dd
AndresMQ627 <andres.190822@gmail.com>
2025-10-09 14:06:04 -0700

    tarea

--- javaScript/introduccion.js
+++ javaScript/introduccion.js
@@ -37,3 +37,80 @@ for (let i = 1; i <= 5; i++) {
     
     getLista(i)
 }
+
+setTimeout(()=>{
+    document.getElementById("preloader").style.display = "none"
+
+},1000)
+
+const imprimir =(arr)=>{
+    var todo = "";
+    arr.forEach(item=>{
+        todo+=`
+            <li>
+                <img src="https://cdn.thesimpsonsapi.com/500${item.personaje.portrait_path}" width="200px">
+                <h2>${item.personaje.name}</h2>
+            </li>   `
+    })
+    ul.innerHTML=todo
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const selectElement = document.getElementById('opciones');
+
+    selectElement.addEventListener('change', (event) => {
+        const selectedValue = event.target.value;
+        console.log('Valor seleccionado:', selectedValue);
+        if (selectedValue === 'linkEdad') {
+            
+            let res = arr_original.filter((item) => item.personaje.age !== null && item.personaje.age < 18);
+            console.log(res);
+            imprimir(res);
+        } else if (selectedValue === 'mostrarTodo') {
+            imprimir(arr_original);
+        }else if (selectedValue === 'fallecidos') {
+            let res = arr_original.filter((item) => item.personaje.status !== null && item.personaje.status == "Deceased");
+            console.log(res);
+            imprimir(res);
+        }else if (selectedValue === 'opea') {
+            let res = arr_original
+            .filter((item) => item.personaje.age !== null) 
+            .sort((a, b) => a.personaje.age - b.personaje.age); 
+            console.log(res);
+             imprimir(res);
+        }else if (selectedValue === 'viejoYjoven') {
+            let res = arr_original.filter((item) => item.personaje.age !== null); 
+
+            if (res.length > 0) {
+                let maxAge = Math.max(...res.map((item) => item.personaje.age)); 
+                let minAge = Math.min(...res.map((item) => item.personaje.age)); 
+
+                let oldestAndYoungest = res.filter((item) => 
+                    item.personaje.age === maxAge || item.personaje.age === minAge
+                ); 
+
+                imprimir(oldestAndYoungest);}
+        }else if (selectedValue === 'promedio') {
+            let res = arr_original.filter((item) => item.personaje.age !== null); 
+
+            if (res.length > 0) {
+                let totalEdad = res.reduce((sum, item) => sum + item.personaje.age, 0); 
+                let promedio = totalEdad / res.length; 
+                alert(`Promedio de edades: ${promedio.toFixed(2)}`);
+
+               
+            } 
+        }else if (selectedValue === 'Estudiantes') {
+            const text ="Student";
+            let res = arr_original.filter((item) => item.personaje.occupation.include(text));
+            console.log(res);
+            imprimir(res);
+        }
+      
+    });
+});
+
+
+
+
