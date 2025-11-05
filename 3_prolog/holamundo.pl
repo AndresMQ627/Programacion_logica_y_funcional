6f2121e098a730c19e1a07211d8d49404fc22bfd
AndresMQ627 <andres.190822@gmail.com>
2025-11-05 13:22:06 -0700

    holamundo

---
+++ 3_prolog/holamundo.pl
@@ -0,0 +1,16 @@
+valioso(plata).
+valioso(oro).
+le_gusta(juan,maria).
+tiene(juan,libro).
+tiene(maria,libro).
+progenitor(clara,jose).
+progenitor(tomas,jose).
+progenitor(tomas,isabel).
+progenitor(jose,ana).
+progenitor(jose,patricia).
+progenitor(patricia,jaime).
+
+% ¿Es ana tia de jaime?
+%progenitor(X, ana), progenitor(X, Y), progenitor(Y, jaime).
+%¿Es ana tia de isabel?
+%progenitor(X, ana), progenitor(X, Y), progenitor(Y, isabel).
